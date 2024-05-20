2b2373d6b0aa0e497bb1a011240d66efdd88c1d5
holmesgulnara <holmesgulnara@gmail.com>
2024-05-20 14:13:22 +0000

    changes

--- script.sh
+++ script.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-
 function prepare_bastion() {
     sudo apt update
     sudo apt install ansible -y
-
+    
     if [ ! -f /usr/share/keyrings/hashicorp-archive-keyring.gpg ]; then
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
     fi
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
     sudo apt update && sudo apt install terraform
-
 }
 
 function create_instance() {
@@ -23,7 +20,7 @@ function update_ip() {
     sed -i "s/ansible_host=[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/ansible_host=$IP/" ../ansible/hosts
 }
 
-function ansible(){
+function ansible() {
     cd ../ansible
     ansible-playbook main.yml
 }
@@ -31,4 +28,5 @@ function ansible(){
 prepare_bastion
 create_instance
 update_ip
+sleep 20
 ansible
